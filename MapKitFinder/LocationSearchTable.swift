cdb8d4023ee2ff10efddca6ab439c90553a243c0
Minh Nguyen <anh.minh.anh.nguyen@gmail.com>
2021-07-05 15:51:50 +0700

    Initial commit

---
+++ MapKitFinder/LocationSearchTable.swift
@@ -0,0 +1,79 @@
+//
+//  LocationSearchTable.swift
+//  MapKitFinder
+//  Created by Minh Nguyen on 05/07/2021.
+//  Copyright © 2021 Minh Nguyen. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationSearchTable : UITableViewController {
+    var handleMapSearchDelegate:HandleMapSearch? = nil
+    var matchingItems:[MKMapItem] = []
+    var mapView: MKMapView? = nil
+    
+    func parseAddress(selectedItem:MKPlacemark) -> String {
+        // put a space between "4" and "5"
+        let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil) ? " " : ""
+        // put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        // put a space between "1" and "2"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            // street number
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            // street name
+            selectedItem.thoroughfare ?? "",
+            comma,
+            // city
+            selectedItem.locality ?? "",
+            secondSpace,
+            // state
+            selectedItem.administrativeArea ?? ""
+        )
+        return addressLine
+    }
+}
+
+extension LocationSearchTable : UISearchResultsUpdating {
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        guard let mapView = mapView,
+            let searchBarText = searchController.searchBar.text else { return }
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        search.startWithCompletionHandler { response, _ in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension LocationSearchTable {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = parseAddress(selectedItem)
+        return cell
+    }
+}
+
+extension LocationSearchTable {
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let selectedItem = matchingItems[indexPath.row].placemark
+        handleMapSearchDelegate?.dropPinZoomIn(selectedItem)
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
